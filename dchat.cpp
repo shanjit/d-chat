69fc09820dc69b7e13babd1888b1df184bfb5fb0
Rohit Dureja <rohit@seas.upenn.edu>
2014-04-14 12:01:29 -0400

    added user initialization

---
+++ dchat.cpp
@@ -0,0 +1,126 @@
+#include <iostream>
+#include <thread>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <netdb.h>
+#include <unistd.h>
+
+using namespace std;
+
+#define LEADER 100
+#define OTHER 101
+
+#define SA struct sockaddr
+
+#define BUFLEN 512
+
+char operating_mode;
+char username[16];
+
+int main(int argc, char *argv[])
+{
+	if(argc<2)
+	{
+		cout << "usage: \t if initiating chat: " << argv[0] << " <username>" << endl;
+		cout << " \t if connecting to group : " << argv[0] << " <usernamename> <ip>:<port>" << endl;
+		return 0;
+	}
+	else if(argc==2)
+	{
+		operating_mode = LEADER;
+		strncpy(username, argv[1], 16);
+	}
+	else if(argc == 3)
+	{
+		operating_mode = OTHER;
+		strncpy(username, argv[1], 16);
+	}
+	else
+	{
+		cout << "usage: \t if initiating chat: " << argv[0] << " <name>" << endl;
+		cout << " \t if connecting to group : " << argv[0] << " <name> <ip of member>:<port of member>" << endl;
+		return 0;
+	}
+
+	// --- find out default IP used for communication ---- //
+	const char* google_dns_server = "8.8.8.8";
+    int dns_port = 53;
+    struct sockaddr_in serv; 
+    int sock = socket (AF_INET, SOCK_DGRAM, 0);
+    if(sock < 0)
+    {
+        perror("Socket error");
+    }
+    memset(&serv, 0, sizeof(serv));
+    serv.sin_family = AF_INET;
+    serv.sin_addr.s_addr = inet_addr(google_dns_server);
+    serv.sin_port = htons(dns_port);
+    int err = connect(sock , (const struct sockaddr*) &serv , sizeof(serv));
+    struct sockaddr_in name;
+    socklen_t namelen = sizeof(name);
+    err = getsockname(sock, (struct sockaddr*) &name, &namelen); 
+    char buffer[100];
+    const char* p = inet_ntop(AF_INET, &name.sin_addr, buffer, 100);   
+    if(p == NULL)
+    {
+        //Some error
+        printf ("Error number : %d . Error message : %s \n" , errno , strerror(errno));
+    }
+    close(sock);
+
+    // --- create and bind socket for communication --- //
+	int sockfd;
+	char datagram[BUFLEN];
+	struct sockaddr_in cliaddr, leaderaddr;
+
+	if(operating_mode == LEADER)
+	{
+		socklen_t len = sizeof(leaderaddr);
+		char servip[20];
+		sockfd = socket(AF_INET, SOCK_DGRAM, 0);
+		// --- clear out memory and assign IP parameters --- //
+		memset((char *) &leaderaddr, 0, sizeof(leaderaddr));
+		leaderaddr.sin_family = AF_INET;
+		leaderaddr.sin_addr.s_addr = inet_addr(buffer);
+		leaderaddr.sin_port = htons(INADDR_ANY);
+		// --- bind socket --- //
+		bind(sockfd, (SA *) &leaderaddr, sizeof(leaderaddr));
+    	err = getsockname(sock, (SA *) &leaderaddr, &len);
+    	// --- print out initialization status --- //
+    	inet_ntop(AF_INET, &leaderaddr.sin_addr, servip, 20);
+    	cout << argv[1] << " started a new chat, listening on " << servip << ":" << ntohs(leaderaddr.sin_port) << endl;
+	}
+	else if(operating_mode == OTHER)
+	{
+		char servip[20];
+		char servport[20];
+		char cliip[20];
+		socklen_t len = sizeof(cliaddr);
+		sscanf(argv[2], "%15[^:]:%s", servip, servport);
+
+		// --- leader information --- //
+		// --- clear out memory and assign IP parameters --- //
+		memset((char *) &leaderaddr, 0, sizeof(leaderaddr));
+		leaderaddr.sin_family = AF_INET;
+		inet_pton(AF_INET, servip, &leaderaddr.sin_addr);		
+		leaderaddr.sin_port = htons(atoi(servport));
+
+		// --- client initialization --- //
+		sockfd = socket(AF_INET, SOCK_DGRAM, 0);
+		// --- clear out memory and assign IP parameters --- //
+		memset((char *) &cliaddr, 0, sizeof(cliaddr));
+		cliaddr.sin_family = AF_INET;
+		cliaddr.sin_addr.s_addr = inet_addr(buffer);
+		cliaddr.sin_port = htons(INADDR_ANY);
+		// --- bind socket --- //
+		bind(sockfd, (SA *) &cliaddr, sizeof(cliaddr));
+    	err = getsockname(sock, (SA *) &cliaddr, &len);
+    	// --- print out initialization status --- //
+    	inet_ntop(AF_INET, &leaderaddr.sin_addr, servip, 20);
+    	inet_ntop(AF_INET, &cliaddr.sin_addr, cliip, 20);
+    	cout << argv[1] << " joining a new chat on " << servip << ":" << ntohs(leaderaddr.sin_port) << ", listening on " << cliip << ":" << ntohs(cliaddr.sin_port) << endl;
+	}
+	return 0;
+}
